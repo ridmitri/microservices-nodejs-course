a9933e124416fd902d2e87b7ee3ffb6709eda86b
Dmitry Ryzhov <ridmitri@gmail.com>
2023-03-29 12:14:04 +0200

    Setup test environment

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,13 @@
+import { it } from 'vitest';
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '1234',
+    })
+    .expect(201);
+});
