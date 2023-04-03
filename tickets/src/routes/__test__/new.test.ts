88e3a9d51ea5ec830ce79fc6cd2a2667149b3604
Dmitry Ryzhov <ridmitri@gmail.com>
2023-04-03 10:53:29 +0200

    Add ticket schema and its test

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,27 +1,81 @@
 import { it, expect } from 'vitest';
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets/ for post requests', async () => {
-  const response = await request(app).post('/api/tickets/new').send({});
+  const response = await request(app).post('/api/tickets').send({});
   expect(response.status).not.toBe(404);
 });
 
 it('can only be accessed if the user is logged in', async () => {
-  return request(app).post('/api/tickets/new').send({}).expect(401);
+  return request(app).post('/api/tickets').send({}).expect(401);
 });
 
 it('returns a status other than 401 if the user is logged in', async () => {
   const response = await request(app)
-    .post('/api/tickets/new')
+    .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({});
 
   expect(response.status).not.toBe(401);
 });
 
-it('returns an error if invalid title is provided ', async () => {});
+it('returns an error if invalid title is provided ', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
 
-it('returns an error if invalid price is provided ', async () => {});
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
 
-it('creates a tickets for valid inputs ', async () => {});
+it('returns an error if invalid price is provided ', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'Valid title',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'Valid title',
+    })
+    .expect(400);
+});
+
+it('creates a tickets for valid inputs ', async () => {
+  let tickets = await Ticket.find({});
+
+  expect(tickets.length).toEqual(0);
+
+  const title = 'Valid title';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price: 20,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+});
