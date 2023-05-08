51a02028268579c48669124231102db2d0ee0c91
Dmitry Ryzhov <ridmitri@gmail.com>
2023-05-08 14:27:17 +0200

    Add NATS events

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,29 @@
+import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
+console.clear();
+
+const stan = nats.connect('ticketing', 'abc', {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Publisher connected to NATS');
+
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
+
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
+    id: '123',
+    title: 'concert',
+    price: 10,
+  });
+});
