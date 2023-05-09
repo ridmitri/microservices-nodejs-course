da394c2a0e0c5861b8485ddd84496cd6a465d767
Dmitry Ryzhov <ridmitri@gmail.com>
2023-05-09 22:32:30 +0200

    Test events with nats-test listener

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,14 +1,18 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
-import { Subjects } from './subjects';
 import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: unknown, msg: Message) {
-    console.log('event data', data);
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    console.log('Event data!', data);
+
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
 
     msg.ack();
   }
