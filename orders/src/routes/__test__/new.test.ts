70c26b1e9385a4b5a3b4a504f611745dc731521c
Dmitry Ryzhov <ridmitri@gmail.com>
2023-05-12 14:09:28 +0200

    Add test for new order route

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+import { it, expect } from 'vitest';
+import testapi from '@/test/setup';
+import { Order, OrderStatus } from '@/models/order';
+import { Ticket } from '@/models/ticket';
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await testapi
+    .post('/api/orders')
+    .send({
+      ticketId,
+    })
+    .expect(404);
+});
+
+it('returns an error if the ticket is reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: 'test-user',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+
+  await order.save();
+
+  await testapi
+    .post('/api/orders')
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(400);
+});
+
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+  const { body } = await testapi
+    .post('/api/orders')
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  expect(body.ticket.id).toEqual(ticket.id);
+  expect(body.ticket.title).toEqual(ticket.title);
+  expect(body.ticket.price).toEqual(ticket.price);
+});
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,18 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@unlimited-js/common';
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+  BadRequestError,
+} from '@unlimited-js/common';
 import { body } from 'express-validator';
-// import { Ticket } from '@/models/ticket';
+import { Ticket } from '@/models/ticket';
+import { Order, OrderStatus } from '@/models/order';
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   '/api/orders',
@@ -21,8 +29,30 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    // const tickets = await Ticket.find({});
-    res.send({});
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved.');
+    }
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+    res.status(201).send(order);
   }
 );
 
